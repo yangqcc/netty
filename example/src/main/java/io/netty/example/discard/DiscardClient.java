800018c9d9d3ab80dfbee5d536698217899b034b
yangqc <yangqicheng@daianla.cn>
2019-01-08 17:40:34 +0800

    格式化代码

--- example/src/main/java/io/netty/example/discard/DiscardClient.java
+++ example/src/main/java/io/netty/example/discard/DiscardClient.java
@@ -34,9 +34,9 @@ import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
  */
 public final class DiscardClient {
 
-    static final boolean SSL = System.getProperty("ssl") != null;
-    static final String HOST = System.getProperty("host", "127.0.0.1");
-    static final int PORT = Integer.parseInt(System.getProperty("port", "8009"));
+    private static final boolean SSL = System.getProperty("ssl") != null;
+    private static final String HOST = System.getProperty("host", "127.0.0.1");
+    private static final int PORT = Integer.parseInt(System.getProperty("port", "8009"));
     static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
 
     public static void main(String[] args) throws Exception {
